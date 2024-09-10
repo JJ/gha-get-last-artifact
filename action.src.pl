69615cf1506230bcd63bdc7dad4e6fdb88cbd197
JJ Merelo <jjmerelo@gmail.com>
2024-09-10 09:37:12 +0200

    :construction:

--- action.src.pl
+++ action.src.pl
@@ -20,18 +20,15 @@ my $request = makeRequest($url, $GITHUB_TOKEN );
 my $response;
 
 eval { $response = decode_json( $ua->request($request)->decoded_content ) } || die "Can't decode $!";
-say "«$url» all good ";
 my $download_url = $response->{'artifacts'}->[0]->{'archive_download_url'};
 
 my $artifact_request = makeRequest($download_url, $GITHUB_TOKEN );
 $ua->max_redirect(0);
 eval { $response = $ua->request( $artifact_request ) } || die "Can't download $download_url: $!";
 
-say "«$download_url» ", $response->status_line;
 my $actual_download_url = $response->header('Location');
-say "Location $actual_download_url";
 
-`curl -o file.zip $actual_download_url `;
+`curl -o file.zip $actual_download_url && ls`;
 `unzip file.zip`;
 
 
